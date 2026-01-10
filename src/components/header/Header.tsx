38b963c6ab2089cabbdfe0f26e4b77452076b0d9
Eze-sk <ezequielsmiak@gmail.com>
2026-01-10 10:27:07 -0300

    feat: add logic for sterilization by streak level

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,16 +4,69 @@ import { TrainingDay, UserMetadataType } from "../../types/UserMetadata";
 import StreakMenu from "./StreaksMenu";
 import MenuIcon from "../../assets/Menu";
 import UserMenu from "./UserMenu";
+import { isWithinInterval, set } from "date-fns";
 
 export interface Props {
   metadata: UserMetadataType | null
   weekDays: TrainingDay[] | null
 }
 
+export interface StreakLevelType {
+  style: string,
+  message: string,
+  shortMessage?: string,
+}
+
+export type StreakLevel = "initial" | "constancy" | "presidency" | "advanced" | "dangerous"
+
 export default function Header({ metadata, weekDays }: Props) {
   const [openStreakMenu, setOpenStreakMenu] = useState(false)
   const [openUserMenu, setOpenUserMenu] = useState(false)
   const menuRef = useRef<HTMLDivElement>(null)
+
+  const streakLevelStyles: Record<StreakLevel, StreakLevelType> = {
+    initial: {
+      style: "text-txt-secondary",
+      message: "Get started now and activate your streak today!",
+      shortMessage: "No active streak",
+    },
+    constancy: {
+      style: "text-br-primary",
+      message: "Good rhythm! Stay consistent and don't stop.",
+      shortMessage: `${metadata?.current_streak} day streak`,
+    },
+    presidency: {
+      style: "text-txt-primary",
+      message: "Total dominance! You are forging a habit of steel.",
+      shortMessage: `${metadata?.current_streak} day streak 🔥`,
+    },
+    advanced: {
+      style: "text-acc-primary",
+      message: "Advanced level! Your discipline is an inspiration.",
+      shortMessage: `Incredible! ${metadata?.current_streak} days`,
+    },
+    dangerous: {
+      style: "text-red-500",
+      message: "Careful! Don't break the cycle now, you're at the top.",
+      shortMessage: `Epic streak of ${metadata?.current_streak}!`,
+    }
+  }
+
+  const date = new Date()
+  const startDangerousTime = set(date, { hours: 21, minutes: 0, seconds: 0 })
+  const endDangerousTime = set(date, { hours: 23, minutes: 0, seconds: 0 })
+
+  const getLevelKey = (days: number) => {
+    if (isWithinInterval(date, { start: startDangerousTime, end: endDangerousTime })) return "dangerous"
+    else if (days === 0) return "initial";
+    else if (days <= 7) return "constancy";
+    else if (days <= 15) return "presidency";
+    else if (days <= 30) return "advanced";
+    return "dangerous";
+  };
+
+  const currentLevel = streakLevelStyles[getLevelKey(metadata?.current_streak ?? 0)];
+  const maxLevel = streakLevelStyles[getLevelKey(metadata?.max_streak ?? 0)].style;
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
@@ -34,15 +87,28 @@ export default function Header({ metadata, weekDays }: Props) {
     <header className="m-5 absolute left-0 right-0 flex items-center justify-between">
       <div className="relative">
         <button
-          className="flex items-center gap-4 cursor-pointer"
+          className={`
+            flex items-center gap-4 cursor-pointer bg-layer-mid py-2 px-4 rounded
+            border border-br-primary ${currentLevel.style} hover:bg-acc-primary
+          `}
           onClick={() => setOpenStreakMenu(!openStreakMenu)}
+          title={currentLevel.shortMessage}
         >
           <Flame />
-          <span>
+          <span className="font-bold">
             {metadata?.current_streak}
           </span>
         </button>
-        <StreakMenu menuRef={menuRef} metadata={metadata} openMenu={openStreakMenu} weekDays={weekDays} />
+        <StreakMenu
+          menuRef={menuRef}
+          metadata={metadata}
+          openMenu={openStreakMenu}
+          weekDays={weekDays}
+          style={{
+            currentLevel,
+            maxLevel
+          }}
+        />
       </div>
       <div className="relative">
         <button
--- src/components/header/StreaksMenu.tsx
+++ src/components/header/StreaksMenu.tsx
@@ -2,32 +2,52 @@ import { RefObject } from "react";
 import Flame from "../../assets/Flame";
 import { CalendarDays, CalendarDaysShow } from "../CalendarDays";
 import { TrainingDay, UserMetadataType } from "../../types/UserMetadata";
+import { StreakLevelType } from "./Header";
 
 export interface Props {
   openMenu: boolean;
   menuRef: RefObject<HTMLDivElement | null>
   metadata: UserMetadataType | null
   weekDays: TrainingDay[] | null
+  style?: {
+    currentLevel: StreakLevelType,
+    maxLevel: string
+  }
 }
 
-export default function StreakMenu({ openMenu, menuRef, metadata, weekDays }: Props) {
+export default function StreakMenu({ openMenu, menuRef, metadata, weekDays, style }: Props) {
+  let currentLevel: StreakLevelType
+  let maxLevel: string
+
+  if (style) {
+    currentLevel = style.currentLevel
+    maxLevel = style.maxLevel
+  } else {
+    currentLevel = {
+      style: "text-txt-secondary",
+      message: "Complete the routine and start your new streak",
+    }
+
+    maxLevel = "text-txt-secondary"
+  }
+
   return (
     <>
       {openMenu && (
         <div
           role="dialog"
           ref={menuRef}
-          className="absolute top-8.75 border border-br-primary bg-layer-mid p-4 z-50"
+          className="absolute top-15 border border-br-primary bg-layer-mid p-4 z-50"
           style={{ borderRadius: "0 15px 15px 15px" }}
         >
           <img src="./chevron-card.svg" alt="chevron" className="absolute -top-2.5 left-1" />
           <div className="flex gap-2 mb-5 items-center justify-center">
             <div>
-              <h2 className="text-3xl font-bold">{metadata?.current_streak} day streak</h2>
-              <h3 className="text-xl font-medium text-txt-secondary mt-1">Your longest streak {metadata?.max_streak}</h3>
-              <p className="text-lg mt-3 max-w-[24ch] text-pretty">Complete the routine and start your new streak</p>
+              <h2 className={`text-3xl font-bold ${currentLevel.style}`}>{metadata?.current_streak} day streak</h2>
+              <h3 className={`text-xl font-medium mt-1 ${maxLevel}`}>Your longest streak {metadata?.max_streak}</h3>
+              <p className="text-lg mt-3 max-w-[24ch] text-pretty">{currentLevel.message}</p>
             </div>
-            <Flame width={150} />
+            <Flame width={150} className={`${currentLevel.style}`} />
           </div>
           <div className="bg-layer-top border border-br-primary rounded">
             <CalendarDays
