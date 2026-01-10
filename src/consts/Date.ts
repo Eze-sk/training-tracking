51d5db74313eb2ab1924422464f44979b547dd55
Eze-sk <ezequielsmiak@gmail.com>
2026-01-10 13:49:04 -0300

    style: format code with prettier

--- src/consts/Date.ts
+++ src/consts/Date.ts
@@ -1,2 +1,15 @@
-export const DAY = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
-export const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+export const DAY = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+export const MONTHS = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+]
